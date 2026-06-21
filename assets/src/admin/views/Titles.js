a661d44347ca47289f067c2cccac1d8d8566a22b
Alexander Caicedo <a.caicedo.dev@gmail.com>
2026-06-21 14:32:47 -0500

    feat(admin): per-content-type and per-taxonomy templates in Titles view

--- assets/src/admin/views/Titles.js
+++ assets/src/admin/views/Titles.js
@@ -1,6 +1,12 @@
 import { TextControl, TextareaControl } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { SettingsPanel } from '../components/SettingsPanel';
+import { TemplateGroup } from '../components/TemplateGroup';
+
+const contentTypes = window.openseoAdmin?.contentTypes ?? {
+	postTypes: [],
+	taxonomies: [],
+};
 
 export function Titles() {
 	return (
@@ -13,21 +19,6 @@ export function Titles() {
 						onChange={ ( v ) => change( 'title_separator', v ) }
 					/>
 					<TextControl
-						label={ __( 'Default title template', 'openseo' ) }
-						value={ values.title_template }
-						onChange={ ( v ) => change( 'title_template', v ) }
-					/>
-					<TextareaControl
-						label={ __(
-							'Default description template',
-							'openseo'
-						) }
-						value={ values.description_template }
-						onChange={ ( v ) =>
-							change( 'description_template', v )
-						}
-					/>
-					<TextControl
 						label={ __( 'Homepage title', 'openseo' ) }
 						value={ values.home_title }
 						onChange={ ( v ) => change( 'home_title', v ) }
@@ -36,6 +27,22 @@ export function Titles() {
 						label={ __( 'Homepage description', 'openseo' ) }
 						value={ values.home_description }
 						onChange={ ( v ) => change( 'home_description', v ) }
+					/>
+
+					<h2>{ __( 'Content types', 'openseo' ) }</h2>
+					<TemplateGroup
+						types={ contentTypes.postTypes }
+						mapKey="post_types"
+						values={ values }
+						change={ change }
+					/>
+
+					<h2>{ __( 'Taxonomies', 'openseo' ) }</h2>
+					<TemplateGroup
+						types={ contentTypes.taxonomies }
+						mapKey="taxonomies"
+						values={ values }
+						change={ change }
 					/>
 				</>
 			) }
